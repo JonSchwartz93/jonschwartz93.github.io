8d877daba137849eddef1ae59a083b67d080f9e3
JonSchwartz93 <josch@umich.edu>
2020-01-12 22:23:36 -0500

    npm install serve and create build for 2019-01-12

---
+++ build/precache-manifest.357b8eb257417d8f199b3a09398a2019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387a7fda5cbf98e7f8bcd69fb592be27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ed9efefe0dcce674e7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9c0d1bcbbd9662672e74",
+    "url": "/static/js/2.6d334f58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6d334f58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28ed9efefe0dcce674e7",
+    "url": "/static/js/main.144fc59f.chunk.js"
+  },
+  {
+    "revision": "d1a19a062f5214fe4145",
+    "url": "/static/js/runtime-main.ebe23bff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
